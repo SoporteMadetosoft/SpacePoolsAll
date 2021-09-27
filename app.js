07c18db49a7ca4b3742955a6fe5811b051ebe635
XaviMadetosoft <80711143+XaviMadetosoft@users.noreply.github.com>
2021-09-27 09:28:02 +0200

    Build

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const path = require('path');
 const fileUpload = require('express-fileupload');
 
-dotenv.config({ path: './.envLocal' })
+dotenv.config({ path: './.env' })
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -69,9 +69,9 @@ app.use('/calendar', require('./routes/calendar'));
 //app.use('/roles', require('./routes/role'));  
 //-------------------------------------------------------
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname + '/ux/build/index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/ux/build/index.html'));
+});
 
 const server = app.listen(port, () => {
   console.log(`app listening at ${origin}:${port}`);

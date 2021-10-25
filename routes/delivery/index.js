839d41fb42907a29a876fdfc059fc9736523fec9
XaviMadetosoft <80711143+XaviMadetosoft@users.noreply.github.com>
2021-10-25 12:50:19 +0200

    Delivery

--- routes/delivery/index.js
+++ routes/delivery/index.js
@@ -10,8 +10,8 @@ const router = express.Router();
 
 router.get('/list', Controller.list);
 router.post('/find', Controller.listByID);
-// router.post('/insert', Controller.insert);
-router.put('/update', Controller.insert);
+router.post('/insert', Controller.insert);
+router.put('/update', Controller.update);
 
 
 module.exports = router;

d071c1fe26be4745dc6a805be16d4452929ec7c0
XaviMadetosoft <80711143+XaviMadetosoft@users.noreply.github.com>
2021-10-26 08:13:50 +0200

    Build

--- dao/AuthDao.js
+++ dao/AuthDao.js
@@ -3,6 +3,7 @@ const StatusDao = require("./global/StatusDao");
 
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+const RoleDao = require("./role/RoleDao");
 const saltRounds = 10;
 
 class AuthDao {
@@ -10,6 +11,7 @@ class AuthDao {
 
     constructor() {
         this.StatusDao = new StatusDao()
+        this.RoleDao = new RoleDao()
     }
     findUser(email, password) {
         return new Promise((resolve, reject) => {
@@ -36,65 +38,88 @@ class AuthDao {
     }
 
     async mountObj(data) {
+
+        const role = await this.RoleDao.findById(data.idRole)
+        const roleName = role.name
+
+        delete role.id
+        delete role.name
+        delete role.productionStatus
+
         const accessToken = jwt.sign({ id: data.id }, process.env.JWT_SECRET)
         const refreshToken = jwt.sign({ id: data.id }, process.env.JWT_SECRET_REFRESH)
         const user = {
             userData: {
                 ...data,
-                role: 'Admin',
+                role: roleName,
                 ability: [
                     {
                         action: 'manage',
                         subject: 'all'
                     }
                 ]
-                // role: await this.RoleDao.getAllRole(data.roleId)
+                // ability: this.tratarRole(role)
             },
             accessToken,
             refreshToken
         }
-
         return user
+    }
+
+    capitalizeFirstLetter(string) {
+        return string.charAt(0).toLowerCase() + string.slice(1);
+    }
+
+    tratarRole(role) {
+        const newObjectRole = []
+        Object.entries(role).map(el => {
+            Object.entries(el[1]).map(perm => {
+                if (perm[1] === true) {
+                    newObjectRole.push({ subject: this.capitalizeFirstLetter(el[0]), action: perm[0] })
+                }
+            })
+        })
+        return newObjectRole
     }
 }
 
 module.exports = AuthDao
 
 
-// ,
-//                     {
-//                         action: 'insert',
-//                         subject: 'customers'
-//                     },
-//                     {
-//                         action: 'update',
-//                         subject: 'customers'
-//                     },
-//                     {
-//                         action: 'delete',
-//                         subject: 'customers'
-//                     },
-//                     {
-//                         action: 'actions',
-//                         subject: 'customers'
-//                     },
-//                     {
-//                         action: 'read',
-//                         subject: 'carriers'
-//                     },
-//                     {
-//                         action: 'insert',
-//                         subject: 'carriers'
-//                     },
-//                     {
-//                         action: 'update',
-//                         subject: 'carriers'
-//                     },
-//                     {
-//                         action: 'delete',
-//                         subject: 'carriers'
-//                     },
-//                     {
-//                         action: 'actions',
-//                         subject: 'carriers'
-//                     }
+    // ,
+                //     {
+                //         action: 'insert',
+                //         subject: 'customers'
+                //     },
+                //     {
+                //         action: 'update',
+                //         subject: 'customers'
+                //     },
+                //     {
+                //         action: 'delete',
+                //         subject: 'customers'
+                //     },
+                //     {
+                //         action: 'actions',
+                //         subject: 'customers'
+                //     },
+                //     {
+                //         action: 'read',
+                //         subject: 'carriers'
+                //     },
+                //     {
+                //         action: 'insert',
+                //         subject: 'carriers'
+                //     },
+                //     {
+                //         action: 'update',
+                //         subject: 'carriers'
+                //     },
+                //     {
+                //         action: 'delete',
+                //         subject: 'carriers'
+                //     },
+                //     {
+                //         action: 'actions',
+                //         subject: 'carriers'
+                //     }

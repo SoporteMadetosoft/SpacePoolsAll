8b86a88c6c47493ccea44c01b8f1417c78e14173
XaviMadetosoft <80711143+XaviMadetosoft@users.noreply.github.com>
2021-11-10 08:05:40 +0100

    Switch start

---
+++ ux/src/utility/helpers/Axios/switchStart.js
@@ -0,0 +1,8 @@
+import axios from "axios"
+import { endPoints } from "@fixed/endPoints"
+
+export const switchStart = async (id, endPoint) => {
+    console.log(`${process.env.REACT_APP_HOST_URI}${endPoints[endPoint]}/switchStart/${id}`)
+    await axios.post(`${process.env.REACT_APP_HOST_URI}${endPoints[endPoint]}/switchStart/${id}`)
+
+}

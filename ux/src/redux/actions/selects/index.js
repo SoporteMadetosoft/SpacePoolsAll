b332797e129d20f00534499cf7d028790fa6084b
Madetosoft <85953077+SoporteMadetosoft@users.noreply.github.com>
2022-03-09 09:55:03 +0100

    delete old function

--- ux/src/redux/actions/selects/index.js
+++ ux/src/redux/actions/selects/index.js
@@ -34,25 +34,6 @@ export const startAddSelectStatus = (endPoint, key, labelName = 'name') => {
     }
 }
 
-export const startAddSelectPoolItems = (endPoint, key, labelName = 'name', itemType) => {
-    const nObj = {
-        idVendor: null,
-        itemType
-    }
-
-    return async (dispatch) => {
-        const token = localStorage.getItem('accessToken')
-        const { data: { data } } = await axios.post(`${process.env.REACT_APP_HOST_URI}${endPoints[endPoint]}/listItems`, { nObj }, {
-            headers: {
-                'Content-type': 'application/json',
-                'x-token': token,
-                itemType
-            }
-        })
-        dispatch(addSelectOptions(key, data.map(option => ({ label: option[labelName], value: option.id }))))
-    }
-}
-
 export const startAddSelectOptionTree = (endPoint, key, idNode) => {
     return async (dispatch) => {
         const { data: { data } } = await axios.post(`${process.env.REACT_APP_HOST_URI}${endPoints[endPoint]}/select`, { idNode }, {
@@ -64,7 +45,6 @@ export const startAddSelectOptionTree = (endPoint, key, idNode) => {
         dispatch(addSelectOptions(key, data.map(option => ({ ...option }))))
     }
 }
-
 
 export const cleanSelectOptions = () => ({
     type: selectTypes.cleanSelectOptions

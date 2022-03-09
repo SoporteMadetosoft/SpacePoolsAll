332c2094e1e82c8d9ba7ca8586d806a25698120c
Madetosoft <85953077+SoporteMadetosoft@users.noreply.github.com>
2022-03-09 09:33:41 +0100

    CON-50

--- ux/src/router/routes/centers/Centers.js
+++ ux/src/router/routes/centers/Centers.js
@@ -4,7 +4,7 @@ import { FormScreen } from '../../../views/FormScreen'
 import { ListScreen } from '../../../views/ListScreen'
 
 const base = {
-    title: 'Centers',
+    title: 'Centros',
     endPoint: 'Centers',
     list: {
         columns: CenterList
